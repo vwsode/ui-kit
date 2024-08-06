4a342931f8d01856f13dd435c96b6d0625d1c0f8
vwsode <frscode1@gmail.com>
2024-08-06 21:07:26 +0500

    fix: remove unused types

--- src/components/ProgressBar/types.ts
+++ src/components/ProgressBar/types.ts
@@ -17,21 +17,6 @@ export type ProgressBarProps =
   | (ProgressBarBaseProps & { value: number; isIndeterminate?: false })
   | (ProgressBarBaseProps & { value?: never; isIndeterminate: true });
 
-export type ProgressBarStory = {
-  /**
-   * The visual style of the progress bar.
-   */
-  appearance?: ProgressBarAppearance;
-  /**
-   * Shows the progress bar in an indeterminate state when true.
-   */
-  isIndeterminate?: boolean;
-  /**
-   *
-   */
-  value: number;
-} & TestableComponent;
-
 export type StyledRootProps = {
   appearance: ProgressBarAppearance;
 } & TestableComponent;
