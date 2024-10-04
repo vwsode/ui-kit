92e69e7dc312cfdf2c4ef54f50d6d6e44786f382
vwsode <frscode1@gmail.com>
2024-10-04 11:17:31 +0500

    test(breadcrumbs): add unit tests for main functional

---
+++ src/components/Breadcrumbs/__test__/mocks.ts
@@ -0,0 +1,28 @@
+import type { BreadcrumbsItemProps } from '../types';
+
+export const BREADCRUMBS_ITEMS: BreadcrumbsItemProps<'a'>[] = [
+  {
+    text: 'Breadcrumb 1',
+    href: '#1',
+  },
+  {
+    text: 'Breadcrumb 2',
+    href: '#1',
+  },
+  {
+    text: 'Breadcrumb 3',
+    href: '#1',
+  },
+  {
+    text: 'Breadcrumb 4',
+    href: '#1',
+  },
+  {
+    text: 'Breadcrumb 5',
+    href: '#1',
+  },
+  {
+    text: 'Breadcrumb 6',
+    href: '#1',
+  },
+];
